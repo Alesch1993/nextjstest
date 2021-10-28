9ec49b87574d7c1ddc027bd169cf7d49007beb99
Alexander Schopp <Alexander.Schopp@realcore.de>
2021-10-28 09:56:54 +0200

    dev

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <Head>
-          <title>RealInventory</title>
+          <title>RealInventory DEV</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <Header/>
